ba9a6e786cc9a3c2ec952ba95668cccabfae4142
Tyler Collins <tjscollins@gmail.com>
2018-06-29 16:31:47 -1000

    Model for Product table and associated migrations

--- product_info/tests.py
+++ product_info/tests.py
@@ -1,9 +1,27 @@
 from django.test import TestCase
 from django.urls import resolve
-# from product_info.views import product_page
-
+from product_info.models import Product
 
 # Create your tests here.
-# class ProductPageTest(TestCase):
+class ProductTest(TestCase):
 
-#     def test_
+    def test_saving_and_retrieving_products(self):
+        first_product = Product()
+        first_product.name = 'Kraft Singles'
+        first_product.upc = '1234567890123'
+        first_product.save()
+
+        second_product = Product()
+        second_product.name = 'Breyer\'s Vanilla Ice-cream'
+        second_product.upc = '0987654321123'
+        second_product.save()
+
+        saved_products = Product.objects.all()
+        self.assertEqual(saved_products.count(), 2)
+
+        first_saved_product = saved_products[0]
+        second_saved_product = saved_products[1]
+
+        self.assertEqual(first_product.name, first_saved_product.name)
+        self.assertEqual(second_product.name, second_saved_product.name)
+
